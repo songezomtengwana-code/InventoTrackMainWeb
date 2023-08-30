3c1a6aa386aa8aaefca9bc9d7b5b65adb060b0bb
songezo_mtengwana <songezomtengwana@gmail.com>
2023-08-30 16:55:42 +0200

    hosting updates

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// // export const enviroment = {
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCBj2lRQJi4DW344SvnvdisOQu41o-oERg",
+//   authDomain: "inventotrack-a8be4.firebaseapp.com",
+//   projectId: "inventotrack-a8be4",
+//   storageBucket: "inventotrack-a8be4.appspot.com",
+//   messagingSenderId: "295911770194",
+//   appId: "1:295911770194:web:a72047af48e5d2ce1d778b",
+//   measurementId: "G-Q1450FR9KM"
+// };
+
+// }
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
