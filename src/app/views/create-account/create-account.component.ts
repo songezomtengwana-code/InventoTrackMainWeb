516993601f978e620e49a9e9f977d524c61524d0
songezo_mtengwana <songezomtengwana@gmail.com>
2023-09-02 22:08:09 +0200

    create account UI update

--- src/app/views/create-account/create-account.component.ts
+++ src/app/views/create-account/create-account.component.ts
@@ -1,29 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 interface authenticationOptions {
-  name: string,
-  url: string
-  icon : string
+  name: string;
+  url: string;
+  icon: string;
+}
+
+class userAccountModel {
+  name!: string
+  email!: string
+  password!:string
 }
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.scss']
+  styleUrls: ['./create-account.component.scss'],
 })
-export class CreateAccountComponent {
+export class CreateAccountComponent implements OnInit {
+  
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   authOptions: authenticationOptions[] = [
     {
-      name: "Goolge",
+      name: 'Goolge',
       url: 'https://google.com',
-      icon : 'bi bi-goolge'
+      icon: 'bi bi-goolge',
     },
     {
-      name: "Microsoft",
+      name: 'Microsoft',
       url: 'https://google.com',
-      icon : 'bi bi-micorsoft'
-    }
-  ]
+      icon: 'bi bi-micorsoft',
+    },
+  ];
 
+  handleUserAccountCreation() {
+    
+  }
 }
