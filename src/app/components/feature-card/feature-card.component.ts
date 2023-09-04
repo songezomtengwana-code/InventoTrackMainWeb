b5bba4370227f809a9f5b5a504017e22e470aebf
songezo_mtengwana <songezomtengwana@gmail.com>
2023-09-04 12:47:28 +0200

    feature reg

--- src/app/components/feature-card/feature-card.component.ts
+++ src/app/components/feature-card/feature-card.component.ts
@@ -1,40 +1,44 @@
 import { Component } from '@angular/core';
 
 interface feature {
-  date: string,
-  title: string,
-  body: string,
-  updates: any,
-  new: true,
-  key: string
+  date: string;
+  title: string;
+  body: string;
+  updates: any;
+  new: true;
+  key: string;
 }
 
 @Component({
   selector: 'app-feature-card',
   templateUrl: './feature-card.component.html',
-  styleUrls: ['./feature-card.component.scss']
+  styleUrls: ['./feature-card.component.scss'],
 })
 export class FeatureCardComponent {
-  features:feature[] = [
+  features: feature[] = [
+    {
+      date: '2023 08 10',
+      title: 'Inventory Tracker With Graphs',
+      body: 'lorem ipsum dolor',
+      updates: ['multi-store management'],
+      new: true,
+      key: '5f9r2e4f-f54038d0h-3rriof4eo',
+    },
     {
       date: '2023 07 19',
       title: 'Scan To Find',
       body: 'New scan to find feature to make looking for a store product much easier...',
-      updates: [
-        'multi-store management'
-      ],
+      updates: ['multi-store management'],
       new: true,
-      key: '5f9d4f54f-f54038d0h-5ufb93f4'
+      key: '5f9d4f54f-f54038d0h-5ufb93f4',
     },
     {
       date: '2023 07 02',
       title: 'Mutli Store Integration',
       body: 'lorem ipsum dolor',
-      updates: [
-        'multi-store management'
-      ],
+      updates: ['multi-store management'],
       new: true,
-      key: '5f9d4f54f-f54038d0h-3rriof4eo'
-    }
-  ]
+      key: '5f9d4f54f-f54038d0h-3rriof4eo',
+    },
+  ];
 }
